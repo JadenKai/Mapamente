e3c67ad67f584cefc4d8db0138d7867d10fafc04
xygyl <xygyl@proton.me>
2026-03-22 20:10:22 -0500

    ready for monday

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import router from './routes.js';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const hostname: string = '127.0.0.1';
 const port: number = 3000;
 
 const app = express();
 
-app.use(express.static('.'));
-
-app.get('/', (_req: Request, res: Response) => {
-  res.redirect('/home.html');
-});
+app.set('view engine', 'ejs');
+app.set('views', path.join(dirname, '..', 'views'));
+app.use(express.static(path.join(dirname, '..', 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
