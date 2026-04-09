6c78aa327f12c15e1fa2ac01c9fbcbb93dbbe749
scox3 <scox3@trinity.edu>
2026-04-09 13:55:28 -0500

    Not working just yet but it compiles.

--- models/quizModel.ts
+++ models/quizModel.ts
@@ -1,32 +1,26 @@
+import { ResultSetHeader } from "mysql2";
 import pool from "../config/database.js";
 
-import { type QuestionEntry, type AnswerEntry } from "../types.js";
+import { type QuestionEntry, type AnswerEntry, type Question } from "../types.js";
 
 async function addQuestion(question: QuestionEntry): Promise<boolean> {
-    try {
-        await pool.query("INSERT INTO Question VALUES ? ", [question.questionId,question.cityId,question.questionText]);
-        return true;
-    } catch (err) {
-        console.error("addQuestion Failed: ", err)
-        throw err;
-    }
+    const [result] = await pool.query<ResultSetHeader>("INSERT INTO Question VALUES (?,?,?)", [question.questionId,question.cityId,question.questionText]);
+    return result.affectedRows === 1;
 }
 
 async function getQuestionById(qId: number): Promise<QuestionEntry> {
-    try {
-        const rows = (await pool.query("SELECT * FROM Question WHERE questionId = ?", [qId]))[0];
-        
-        return (rows as unknown as QuestionEntry);
-    } catch(err) {
-        console.error("getQuestionById Failed: ", err)
-        throw err;
-    }
+    const rows = (await pool.query("SELECT * FROM Question WHERE questionId = ?", [qId]))[0];
+    return (rows as unknown as QuestionEntry);
 }
 
-
-
+async function generateQuiz(cityID: number): Promise<Question[]>{
+    const rows = (await pool.query("SELECT * FROM Question WHERE cityId = ? ORDER BY RAND() LIMIT 10;",[cityID]))[0];
+    console.log(rows);
+    return([{questionId:5,questionText:"hi", answers:[]}]);
+}
 
 export {
     addQuestion,
-    getQuestionById
+    getQuestionById,
+    generateQuiz
 }
--- routes/quizRoutes.ts
+++ routes/quizRoutes.ts
@@ -1,9 +1,12 @@
 import { Router, type Request, type Response } from "express";
-import { defaultQuiz, returnQuiz } from "../controllers/quizController.js"
+import * as Controller from "../controllers/quizController.js"
 console.log('quizRoutes loaded');
 const router = Router();
-router.get('/test', defaultQuiz);
-console.log('test route registered'); // add this
-router.post("/test", returnQuiz);
+
+router.get('/test', Controller.produceQuiz);
+
+router.post("/test/:id", Controller.produceQuiz);
+
+router.get("/quiz/:id", Controller.produceQuiz)
 
 export default router;
